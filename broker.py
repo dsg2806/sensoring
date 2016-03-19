e6aaa2ce7db7c4a011f104a3527d343e0c6fa506
dsg2806 <dsg2806@users.noreply.github.com>
2016-03-19 21:33:06 +1100

    Closes DB on disconnect now.

--- broker.py
+++ broker.py
@@ -1,10 +1,11 @@
 import paho.mqtt.client as mqtt
 import sqlite3
+import time
 
 1B3 = sqlite3.connect('1B3.db')
 
 1B3.execute('''CREATE TABLE DATA
-       (ID INT PRIMARY KEY     NOT NULL,
+       (TIME INT PRIMARY KEY     NOT NULL,
        LIGHT           INT    NOT NULL,
        SOUND            INT     NOT NULL,
        TEMPERATURE        INT,
@@ -21,16 +22,18 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     strng = (msg.payload).split(",")
-    idint += 1
     1B3.execute("INSERT INTO 1B3 (ID,LIGHT,SOUND,TEMPERATURE,HUMIDITY) \
-        VALUES (idint, strng[0], strng[1], strng[2], strng[3] )");
+        VALUES (int(time.time()), strng[0], strng[1], strng[2], strng[3] )");
     1B3.commit()
     print(msg.topic+" "+str(msg.payload))
-    #ON CLOSE DO conn.close()
+
+def on_disconnect(client, userdata, rc):
+    1B3.close()
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_disconnect = on_disconnect 
 
 client.connect('118.139.125.241', 1883, 60)
 
