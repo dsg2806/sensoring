c04e5bd3207e36fb6086573c29e4bca1e7308883
dsg2806 <dsg2806@users.noreply.github.com>
2016-03-20 15:05:54 +1100

    Update broker.py

--- broker.py
+++ broker.py
@@ -24,11 +24,17 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     oneB3 = sqlite3.connect('1B3.db')
     strng = (msg.payload).split(",")
-    oneB3.execute(
-	"INSERT INTO DATA (ID,LIGHT,SOUND,TEMPERATURE,HUMIDITY) VALUES (?, ?, ?, ?, ?)"
+    dontRun = False
+    for i in strng:
+    	if (i.isNumeric() == False):
+    		dontRun = True 
+
+    #VALUES (int(time.time()), strng[0], strng[1], strng[2], strng[3] )");
+    if !(dontRun): 
+    	oneB3.execute(
+ 	"INSERT INTO DATA (ID,LIGHT,SOUND,TEMPERATURE,HUMIDITY) VALUES (?, ?, ?, ?, ?)"
 	(int(time.time()), int(strng[0]), int(strng[1]), int(strng[2]), int(strng[3]))
-);
-        #VALUES (int(time.time()), strng[0], strng[1], strng[2], strng[3] )");
+	);
     oneB3.commit()
     oneB3.close()
     print(msg.topic+" "+str(msg.payload))
