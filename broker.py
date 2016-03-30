dcd82c0624b0b398ffa1a5e23108c941c093c2f6
dsg2806 <dsg2806@users.noreply.github.com>
2016-03-30 17:51:18 +1100

    Connections established
    Yet to figure out how to close database properly to read afterwards.

--- broker.py
+++ broker.py
@@ -2,11 +2,12 @@ import paho.mqtt.client as mqtt
 import sqlite3
 import time
 
-oneB3 = sqlite3.connect('1B3.db')
-
-oneB3.execute('''CREATE TABLE DATA (Time INTEGER PRIMARY KEY NOT NULL, Light INTEGER NOT NULL, Sound INTEGER NOT NULL, Temperature INTEGER NOT NULL, 
-Humidity INTEGER NOT NULL);''')
-oneB3.close()
+oneBthree = sqlite3.connect('1B3.db')
+oneBthree.execute('''CREATE TABLE DATA (Time INTEGER PRIMARY KEY NOT
+NULL, Light INTEGER NOT NULL, Sound INTEGER NOT NULL, Temperature
+FLOAT NOT NULL,
+Humidity FLOAT NOT NULL);''')
+oneBthree.close()
 strng = ""
 idint = 0
 
@@ -15,34 +16,37 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("/sensors")
+    client.subscribe("/sensors/1B3")
 
 def on_message(client, userdata, msg):
-    oneB3 = sqlite3.connect('1B3.db')
-    strng = (msg.payload).split(" ")
+    oneBthree = sqlite3.connect('1B3.db')
+    strng = (str(msg.payload)).split(" ")
     dontRun = False
-    for i in strng:
-    	if (i.isNumeric() == False):
-    		dontRun = True 
+    print(str(msg.payload))
 
+#    for i in strng:
+ #      if (i.isdigit() == False):
+  #             dontRun = True
     #VALUES (int(time.time()), strng[0], strng[1], strng[2], strng[3] )");
-    if (dontRun == True): 
-	oneB3.execute("INSERT INTO DATA (Time,Light,Sound,Temperature,Humidity) VALUES (?, ?, ?, ?, ?)"
-	int(time.time()), int(strng[0]), int(strng[1]), int(strng[2]), int(strng[3]))
-
-    oneB3.commit()
-    oneB3.close()
+    if dontRun == False:
+#        oneBthree.execute("INSERT INTO DATA
+(Time,Light,Sound,Temperature,Humidity) VALUES (?,?,?,?,?)",
+(str(time.time()), strng[0] , strng[1] , strng[2], strng[3])
+        oneBthree.execute("insert into data (Time, Light, Sound, Temperature,
+Humidity) values (?, ?, ?, ?, ?)", (int(time.time()), int(strng[0]),
+int(strng[1]), float(strng[2]),float(strng[3])))
+    oneBthree.commit()
+    oneBthree.close()
     print(msg.topic+" "+str(msg.payload))
 
 def on_disconnect(client, userdata, rc):
-    oneB3.close()
+    oneBthree.close()
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.on_disconnect = on_disconnect 
+client.on_disconnect = on_disconnect
 
-client.connect('118.139.125.241', 1883, 60)
+client.connect('localhost', 1883, 60)
 
 client.loop_forever()
-
